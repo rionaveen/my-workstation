86b27f85acc32fd47c0ce00e6d83a61673a92191
rionaveen <naveenbelieve19@gmail.com>
2023-02-11 18:22:38 +0530

    destm

---
+++ Selenium/destm/src/test/java/destm/Stepdef/LoginStep.java
@@ -0,0 +1,37 @@
+package destm.Stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStep {
+
+	@Given("user navigates to the user login page")
+	public void user_navigates_to_the_user_login_page() {
+	   
+	}
+
+	@When("user Enter the Valid Email Address")
+	public void user_enter_the_valid_email_address() {
+	   
+	}
+
+	@And("user Enter The Valid PassWord")
+	public void user_enter_the_valid_pass_word() {
+	   
+	}
+
+	@And("user Click The Login Button")
+	public void user_click_the_login_button() {
+	   
+	}
+
+	@Then("user Validate the home page Title")
+	public void user_validate_the_home_page_title() {
+	 
+	}
+
+
+	
+}
---
+++ Selenium/destm/target/test-classes/destm/feature/Login.feature
@@ -0,0 +1,9 @@
+Feature: faceBook Application
+
+Scenario: login Functionality for valid test data
+
+Given user navigates to the user login page
+When user Enter the Valid Email Address
+And user Enter The Valid PassWord
+And user Click The Login Button
+Then user Validate the home page Title
